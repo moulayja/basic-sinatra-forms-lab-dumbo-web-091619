20186db61bd88012319d58d5914e31bd23afdca1
Jamal Farah <moulayjam@gmail.com>
2019-10-08 17:43:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,12 +8,12 @@ class App < Sinatra::Base
 
   post '/team' do
    name = params["name"]
-   coach = params[:coach]
-   pg = params[:pg]
-   sg = params[:cg]
-   pf = params[:pf]
-   sf = params[:sf]
-   c = params[:c]
+   coach = params["coach"]
+   pg = params["pg"]
+   sg = params["cg"]
+   pf = params["pf"]
+   sf = params["sf"]
+   c = params["c"]
    
     erb :team
 
