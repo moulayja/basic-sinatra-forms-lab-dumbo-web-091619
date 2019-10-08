c54c8e99a88ed8d32cd456ebd4e85a59c5448d6b
Jamal Farah <moulayjam@gmail.com>
2019-10-08 17:55:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
    @name = params["name"]
    @coach = params["coach"]
    @pg = params["pg"]
-   @sg = params["cg"]
+   @sg = params["sg"]
    @pf = params["pf"]
    @sf = params["sf"]
    @c = params["c"]
